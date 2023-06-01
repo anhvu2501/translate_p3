f86f5c526f93bc6f9b6c65ba27a260ab21ad89d8
anhvu2501 <vu25ck28@gmail.com>
2023-06-01 13:38:48 +0700

    update p3_translated

--- p3_translated.py
+++ p3_translated.py
@@ -8,8 +8,7 @@ from multiprocessing import Pool
 
 """
 Commands:
-python3 p3_translated.py --split train --size 1000
-python3 p3_translated.py --split validation --size 100
+python3 p3_translated.py 
 """
 
 translator = Translator(proxies={'http': 'http://localhost:8118'})
@@ -26,13 +25,22 @@ for i in range(len(files)):
     files[i] = files[i].rstrip("\n")
     TASK_LIST.append(files[i])
 
-def read_json(task_name, split):
-    with open(f'p3_{task_name}_{split}.jsonl', 'r') as json_file:
-        json_list = list(json_file)
+def read_json(task_name):
+    with open(f'p3_{task_name}_train.jsonl', 'r') as json_file:
+        train_json_list = list(json_file)
 
-    data_list = []
-    for json_str in json_list:
-        data_list.append(list(json.loads(json_str).values()))
+    with open(f'p3_{task_name}_validation.jsonl', 'r') as json_file:
+        val_json_list = list(json_file)
+
+    train_data_list = []
+    for json_str in train_json_list:
+        train_data_list.append(list(json.loads(json_str).values()))
+
+    val_data_list = []
+    for json_str in val_json_list:
+        val_data_list.append(list(json.loads(json_str).values()))
+    
+    data_list = train_data_list + val_data_list
     return data_list
 
 # List of translated object to list of translated text string
@@ -53,15 +61,15 @@ def list_to_dict(mylist):
     title = ['inputs', 'targets']
     return dict(zip(title, mylist))
 
-def save_translated_json(mydict, task_name, split):
+def save_translated_json(mydict, task_name):
     # save json translated file test
     json_object = json.dumps(mydict, indent=2, ensure_ascii=False)
     
     # Writing to .jsonl
-    with open(f'p3_translated_{task_name}_{split}.jsonl', "w", encoding='utf-8') as outfile:
+    with open(f'p3_translated_{task_name}.jsonl', "w", encoding='utf-8') as outfile:
         outfile.write(json_object)
 
-@retry(wait_fixed=4000)
+@retry(wait_fixed=7000)
 def translate_list_str(translator, list_str):
     return translator.translate(list_str, src='en', dest='vi')
 
@@ -80,30 +88,13 @@ def translate_chunks(list_chunks):
         translated_chunks.append(translated_list_dict_chunk)
     return translated_chunks
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Translate dataset')
-    parser.add_argument(
-        '--split',
-        default='train',
-        help='define split')
-    # chunk size = 1000 when split train
-    # chunk size = 100 when split validation
-    parser.add_argument(
-        '--size',
-        type=int,
-        default=1000,
-        help='define size of chunk to divide'
-    )
-    args = parser.parse_args()
-    return args
 
 def main():
-    args = parse_args()
     FINISHED_TASK_LIST = []
     for task_name in TASK_LIST:
         try:
-            data = read_json(task_name, args.split)
-            data_chunks = list(divide_chunks(data, args.size))
+            data = read_json(task_name)
+            data_chunks = list(divide_chunks(data, 1000))
             print("Starting task...")
             pool_chunks = (data_chunks[0:2], data_chunks[2:4],data_chunks[4:6], data_chunks[6:8], data_chunks[8:])
             with Pool(8) as pool:
@@ -114,8 +105,8 @@ def main():
                 # report a message
                 print('Done translated.')
 
-            save_translated_json(translated_list_1, task_name, args.split)
-            save_translated_json(translated_list_2, task_name, args.split)
+            save_translated_json(translated_list_1, task_name)
+            save_translated_json(translated_list_2, task_name)
         
             # if translated successful add this task_name to the translated list
             FINISHED_TASK_LIST.append(task_name)
